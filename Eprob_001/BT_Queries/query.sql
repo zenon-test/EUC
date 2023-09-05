bbb8a4865b745213f30819522c3d2cad9bea1464
Jack Zhu <zhujielater@gmail.com>
2023-09-05 13:19:26 -0400

    BT query for Eprob_001

--- Eprob_001/BT_Queries/query.sql
+++ Eprob_001/BT_Queries/query.sql
@@ -1,1 +1,26 @@
-SELECT * FROM sample_table; -- Sample BT query
+WITH RedressPayments AS (
+    SELECT
+        l.loan_id,
+        l.customer_id,
+        p.payment_date,
+        p.payment_amount,
+        rr.redress_rate,
+        (p.payment_amount * rr.redress_rate / 100) AS redress_amount
+    FROM
+        loans l
+    JOIN payments p ON l.loan_id = p.loan_id
+    JOIN redress_rates rr ON p.payment_date BETWEEN rr.start_date AND rr.end_date
+)
+
+SELECT
+    loan_id,
+    customer_id,
+    SUM(payment_amount) AS total_payments,
+    SUM(redress_amount) AS total_redress
+FROM
+    RedressPayments
+GROUP BY
+    loan_id,
+    customer_id
+ORDER BY
+    customer_id, loan_id;
